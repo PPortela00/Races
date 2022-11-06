80611bccb0cde61e8eee77fdfce8de71873fc153
PPortela00 <115879660+PPortela00@users.noreply.github.com>
2022-11-06 18:51:34 +0000

    Update question.sql

--- question.sql
+++ question.sql
@@ -1,12 +1,46 @@
+/* 1) How many runners have more than 50 years old?  */
+/* 2) How many runners are in each age class? (age_class, count) */
+/* 3) Which runner has more 1st places? (name, birthdate, count) */
+/* 4) How many runners have more than 50 years old? */
 /*---------------------------5 Questions ---------------------------*/
-/* 1) Who run the fastest 10K race ever (name, birthdate, time) */
-/* 2) What 10K race had the fastest average time (event, event date)? */
-/* 3) What teams had more than 3 participants in the 2016 maratona (team)? */
-/* 4) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
-/* 5) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
+/* 5) Who run the fastest 10K race ever (name, birthdate, time) */
+/* 6) What 10K race had the fastest average time (event, event date)? */
+/* 7) What teams had more than 3 participants in the 2016 maratona (team)? */
+/* 8) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
+/* 9) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
 
 
-/* 1) Who run the fastest 10K race ever (name, birthdate, time) */
+
+
+
+/* 6) How many runners have more than 50 years old? */
+
+
+/* 7) How many runners are in each age class? (age_class, count) */
+SELECT age_class, COUNT(*)
+FROM (SELECT DISTINCT r_id, age_class
+      FROM classification JOIN runner ON runner_id = r_id
+                          JOIN age_class ON class_id = a_id) AS age_classes
+GROUP BY age_class
+
+
+/* 8) Which runner has more 1st places? (name, birthdate, count) */
+SELECT name, b_date, COUNT(*)
+FROM classification JOIN runner ON runner_id = r_id
+WHERE place = '1'
+GROUP BY r_id
+HAVING COUNT(*) >= ALL (SELECT COUNT(*)
+                        FROM classification JOIN runner ON runner_id = r_id
+                        WHERE place = '1'
+                        GROUP BY r_id
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 1)
+
+
+/* 9) What are the events with less then 42km distance? */
+
+/* ---------------------------5 Questions ---------------------------*/
+/* 5) Who run the fastest 10K race ever (name, birthdate, time) */
 SELECT name, b_date, MIN(o_time)
 FROM classification JOIN event ON event_id = e_id
                     JOIN distance ON event.distance = d_id
@@ -21,8 +55,7 @@ WHERE distance.distance = 10
 GROUP BY r_id)
 
 
-
-/* 2) What 10K race had the fastest average time (event, event date)? */
+/* 6) What 10K race had the fastest average time (event, event date)? */
 SELECT event, e_year
 FROM classification JOIN event ON event_id = e_id
                     JOIN distance ON event.distance = d_id
@@ -35,8 +68,7 @@ WHERE distance.distance = 10
 GROUP BY e_id)
 
 
-
-/* 3) What teams had more than 3 participants in the 2016 maratona (team)? */
+/* 7) What teams had more than 3 participants in the 2016 maratona (team)? */
 SELECT team
 FROM classification JOIN event ON event_id = e_id
 WHERE e_year = 2016 AND event.event = 'maratona' AND team <> 'nan'
@@ -44,9 +76,7 @@ GROUP BY team
 HAVING COUNT (*) > 3
 
 
-
-
-/* 4) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
+/* 8) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
 SELECT name, b_date, SUM(distance.distance) AS total_kms
 FROM classification JOIN runner ON runner_id = r_id
                     JOIN event ON event_id = e_id
@@ -64,10 +94,7 @@ LIMIT 5
 ORDER BY SUM(distance.distance) DESC
 
 
-
-
-
-/* 5) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
+/* 9) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
 SELECT name, b_date, GREATEST(dif_1213, dif_1314, dif_1415, dif_1516) AS improvement
 FROM (
 
