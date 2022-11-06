cfd0084b4ef251e953780d0477d499c0e8818b5e
PPortela00 <115879660+PPortela00@users.noreply.github.com>
2022-11-06 19:17:20 +0000

    Udpated Version

--- question.sql
+++ question.sql
@@ -1,12 +1,52 @@
+/* 1) How many runners with more than 70 years of age have signed up in each type of event? (event, desc order count)  */
+/* 2) How many runners are in each age class? (age_class, count) */
+/* 3) Which runner has more 1st places? (name, birthdate, count) */
+/* 4) What are the events with less then 42km distance? (event, distance) */
 /*---------------------------5 Questions ---------------------------*/
-/* 1) Who run the fastest 10K race ever (name, birthdate, time) */
-/* 2) What 10K race had the fastest average time (event, event date)? */
-/* 3) What teams had more than 3 participants in the 2016 maratona (team)? */
-/* 4) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
-/* 5) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
+/* 5) Who run the fastest 10K race ever (name, birthdate, time) */
+/* 6) What 10K race had the fastest average time (event, event date)? */
+/* 7) What teams had more than 3 participants in the 2016 maratona (team)? */
+/* 8) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
+/* 9) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
 
 
-/* 1) Who run the fastest 10K race ever (name, birthdate, time) */
+/* 1) How many runners with more than 70 years of age have signed up in each type of event? (event, desc order count) */
+SELECT event, COUNT(*)
+FROM runner JOIN classification ON r_id = runner_id
+            JOIN event ON event_id = e_id
+WHERE EXTRACT(YEAR FROM age(CURRENT_DATE, runner.b_date)) > 70
+GROUP BY event
+ORDER BY COUNT(*) DESC
+
+
+/* 2) How many runners are in each age class? (age_class, count) */
+SELECT age_class, COUNT(*)
+FROM (SELECT DISTINCT r_id, age_class
+      FROM classification JOIN runner ON runner_id = r_id
+                          JOIN age_class ON class_id = a_id) AS age_classes
+GROUP BY age_class
+
+/* 3) Which runner has more 1st places? (name, birthdate, count) */
+SELECT name, b_date, COUNT(*)
+FROM classification JOIN runner ON runner_id = r_id
+WHERE place = '1'
+GROUP BY r_id
+HAVING COUNT(*) >= ALL (SELECT COUNT(*)
+                        FROM classification JOIN runner ON runner_id = r_id
+                        WHERE place = '1'
+                        GROUP BY r_id
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 1)
+                        
+
+/* 4) What are the events with less then 42km distance? (event, distance) */
+SELECT DISTINCT event, distance.distance
+FROM event JOIN distance ON event.distance = d_id
+WHERE distance.distance < 42
+
+
+/* ---------------------------5 Questions ---------------------------*/
+/* 5) Who run the fastest 10K race ever (name, birthdate, time) */
 SELECT name, b_date, MIN(o_time)
 FROM classification JOIN event ON event_id = e_id
                     JOIN distance ON event.distance = d_id
@@ -22,7 +62,7 @@ GROUP BY r_id)
 
 
 
-/* 2) What 10K race had the fastest average time (event, event date)? */
+/* 6) What 10K race had the fastest average time (event, event date)? */
 SELECT event, e_year
 FROM classification JOIN event ON event_id = e_id
                     JOIN distance ON event.distance = d_id
@@ -36,7 +76,7 @@ GROUP BY e_id)
 
 
 
-/* 3) What teams had more than 3 participants in the 2016 maratona (team)? */
+/* 7) What teams had more than 3 participants in the 2016 maratona (team)? */
 SELECT team
 FROM classification JOIN event ON event_id = e_id
 WHERE e_year = 2016 AND event.event = 'maratona' AND team <> 'nan'
@@ -46,7 +86,7 @@ HAVING COUNT (*) > 3
 
 
 
-/* 4) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
+/* 8) What are the 5 runners with more kilometers in total (name, birthdate, kms)? */
 SELECT name, b_date, SUM(distance.distance) AS total_kms
 FROM classification JOIN runner ON runner_id = r_id
                     JOIN event ON event_id = e_id
@@ -67,7 +107,7 @@ ORDER BY SUM(distance.distance) DESC
 
 
 
-/* 5) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
+/* 9) What was the best time improvement in two consecutive maratona races (name,birthdate, improvement)? */
 SELECT name, b_date, GREATEST(dif_1213, dif_1314, dif_1415, dif_1516) AS improvement
 FROM (
 
@@ -112,46 +152,3 @@ HAVING GREATEST(dif_1213, dif_1314, dif_1415, dif_1516) IS NOT NULL
 ORDER BY GREATEST(dif_1213, dif_1314, dif_1415, dif_1516) DESC
 
 LIMIT 1
-
-
-
-
-/* 6) How many runners with more than 70 years of age have signed up in each type of event? (event, desc order count) */
-SELECT event, COUNT(*)
-FROM runner JOIN classification ON r_id = runner_id
-            JOIN event ON event_id = e_id
-WHERE EXTRACT(YEAR FROM age(CURRENT_DATE, runner.b_date)) > 70
-GROUP BY event
-ORDER BY COUNT(*) DESC
-
-
-
-
-
-
-/* 7) How many runners are in each age class? (age_class, count) */
-SELECT age_class, COUNT(*)
-FROM (SELECT DISTINCT r_id, age_class
-      FROM classification JOIN runner ON runner_id = r_id
-                          JOIN age_class ON class_id = a_id) AS age_classes
-GROUP BY age_class
-
-
-/* 8) Which runner has more 1st places? (name, birthdate, count) */
-SELECT name, b_date, COUNT(*)
-FROM classification JOIN runner ON runner_id = r_id
-WHERE place = '1'
-GROUP BY r_id
-HAVING COUNT(*) >= ALL (SELECT COUNT(*)
-                        FROM classification JOIN runner ON runner_id = r_id
-                        WHERE place = '1'
-                        GROUP BY r_id
-                        ORDER BY COUNT(*) DESC
-                        LIMIT 1)
-
-
-
-/* 9) What are the events with less then 42km distance? (event, distance) */
-SELECT DISTINCT event, distance.distance
-FROM event JOIN distance ON event.distance = d_id
-WHERE distance.distance < 42
